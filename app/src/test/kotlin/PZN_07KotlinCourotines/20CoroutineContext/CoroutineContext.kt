c61158633b8ababb295edc101cacf66542578f14
roynaldi19 <roynaldi19@gmail.com>
2022-02-15 21:31:12 +0700

    update feb 22

--- app/src/test/kotlin/PZN_07KotlinCourotines/20CoroutineContext/CoroutineContext.kt
+++ app/src/test/kotlin/PZN_07KotlinCourotines/20CoroutineContext/CoroutineContext.kt
@@ -1,19 +1,20 @@
 package PZN_07KotlinCourotines.`20CoroutineContext`
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import org.junit.jupiter.api.Test
 import kotlin.coroutines.CoroutineContext
 
 class CoroutineContext {
 
+    @ExperimentalStdlibApi
     @Test
     fun testCoroutineContext(){
         runBlocking {
             val job = GlobalScope.launch {
                 val context: CoroutineContext = coroutineContext
                 println(context)
+                println(context[Job])
+                println(context[CoroutineDispatcher])
             }
             job.join()
         }
